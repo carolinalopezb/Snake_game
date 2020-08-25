4de91cbb0736d3b0e9297dbce41faddcf5986607
carolinalopezb <carolinalopezb@users.noreply.github.com>
2020-08-25 12:56:43 -0300

    finished

--- snake.js
+++ snake.js
@@ -1,12 +1,15 @@
 import { getInputDirection } from "./input.js";
 
-export const SNAKE_SPEED = 1
+export const SNAKE_SPEED = 5
+
+let newSegments = 0
 
 const snakeBody = [
     {x:11 , y: 11},
 ]
 
 export function update(){
+    addSegments()
     const inputDirection = getInputDirection()
     for (let i = snakeBody.length - 2; i >= 0; i--){
         snakeBody[i + 1] = {...snakeBody[i]}
@@ -24,4 +27,33 @@ export function draw(gameBoard){
         snakeElement.classList.add('snake')
         gameBoard.appendChild(snakeElement)
     })
+}
+
+export function expandSnake(amount){
+    newSegments += amount
+}
+
+export function onSnake(position, { ignoreHead = false } = {}){
+    return snakeBody.some((segment, index) =>{
+        if(ignoreHead && index === 0) return false
+        return equalPositions(segment, position)
+    })
+}
+export function getSnakeHead(){
+    return snakeBody[0]
+}
+
+export function snakeIntersection(){
+    return onSnake(snakeBody[0], { ignoreHead: true })
+}
+
+function equalPositions(pos1, pos2){
+    return pos1.x === pos2.x && pos1.y === pos2.y
+}
+
+function addSegments(){
+    for(let i = 0; i < newSegments; i++){
+        snakeBody.push({...snakeBody[snakeBody.length]})
+    }
+    newSegments = 0
 }
